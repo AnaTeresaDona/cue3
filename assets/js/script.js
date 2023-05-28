188d33c193d32b7e0fef6bcddbd3dab7be697bdb
Arquera <anatdona@gmail.com>
2023-05-27 20:45:47 -0400

    Final Drilling Cue3 final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,46 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+const nombreBarraLateral = document.querySelectorAll(".nombreBarraLateral");
+
+nombreBarraLateral.forEach(nombreBarraLateral => {
+    nombreBarraLateral.addEventListener('click', function() {
+  
+      nombreBarraLateral.setAttribute('style', 'font-weight: 400;');
+    });
+});
+
+// persona1.addEventListener('click', function(){    
+//     document.getElementById("persona1").style.fontWeight = "400";
+// })
+
+// persona2.addEventListener('click', function(){    
+//     document.getElementById("persona2").style.fontWeight = "400";
+// })
+
+// persona3.addEventListener('click', function(){    
+//     document.getElementById("persona3").style.fontWeight = "400";
+// })
+
+// persona4.addEventListener('click', function(){    
+//     document.getElementById("persona4").style.fontWeight = "400";
+// })
+
+// persona5.addEventListener('click', function(){    
+//     document.getElementById("persona5").style.fontWeight = "400";
+// })
+
+// persona6.addEventListener('click', function(){    
+//     document.getElementById("persona6").style.fontWeight = "400";
+// })
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     document.getElementById("domi").style.fontWeight = "100";
+//   }
